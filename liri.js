01336f80d97ccf48e3e91eb2d8b6c75250547afe
Megan <mhmeluskey@gmail.com>
2018-10-20 19:59:55 -0400

    finalized project

--- liri.js
+++ liri.js
@@ -7,70 +7,103 @@ var keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify);
 
 var search = process.argv[2];
+console.log(search);
 var term = process.argv.slice(3).join(" ");
 
 /////////////////////////////////////////////////////  
 if (search === "concert-this") {
-   
-    request("https://rest.bandsintown.com/artists/" + term + "/events?app_id=codingbootcamp", function (err, response, body) {
-         
-    
-         if (!err && response.statusCode === 200) {
-            console.log("somthing");
-            console.log("Bands is working");  
-            console.log(term);
-            console.log(response);
-            //name of venue
-            // console.log(JSON.parse(venue.name.response));
-        }//response
 
-       
-        
+  request("https://rest.bandsintown.com/artists/" + term + "/events?app_id=codingbootcamp", function (err, response, body) {
+
+
+    if (!err && response.statusCode === 200) {
+
+      // console.log(term);
+      var info = JSON.parse(body);
+      
+      //console.log(array);
+      console.log(info);
+
+
+      //store all info[0].venue.name in array
+      //run through for loop
+      for (var i = 0; i < info.length; i++) {
+        console.log("Concert" + i);
+
+
+        //console.log(info);
+        //name of venue
+        console.log("Venue: " + info[i].venue.name);
         //venue location
-        //date of event in "mm/dd/yyyy"
+        console.log("Location: " + info[i].venue.city);
+        console.log(info[i].venue.region);
+        //date of the event
+        console.log("Date: " + info[i].datetime);
+      }
 
-    });//bands in town request 
+    }//response
+
+  });//bands in town request 
 }
 
 // ////////////////////////////////////////////////
 if (search === "spotify-this-song") {
-        spotify.search({ type: 'track', query: term }, function(err, data) {
-            if (err) {
-              return console.log('Error occurred: ' + err);
-            } 
-          console.log(data); 
-          console.log(JSON.parse(response));
-            //Artists
-            //Songs name
-            //link of song from spotify
-            //album of song
-            //default = "The Sign" by Ace of Base
-            //find function
-          });
-        }
+  
+ 
+  spotify.search({ type: 'track', query: term }, function (err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+
+    console.log(data.tracks.items);
+//link of song from spotify
+    console.log("Link to song: " + data.tracks.items[0].external_urls.spotify);
+//Artists
+    console.log("artists: " + data.tracks.items[0].artists[0].name);
+//Songs name
+    console.log("song name: " + data.tracks.items[0].name)
+//album of song
+    console.log("album name: " + data.tracks.items[11].name)
+    //default = "The Sign" by Ace of Base
+    //find function
+    
+  });
+}
 
 
 // // /////////////////////////////////////////////////////// 
 
 
 if (search === "movie-this") {
-    request("http://www.omdbapi.com/?t=" + term + "&y=&plot=short&apikey=trilogy", function (err, response, body) {
-      if (!err && response.statusCode === 200) {
-      console.log("Movie information");
-      console.log(term);
-      console.log(JSON.parse(response));
-      
-    
+  request("http://www.omdbapi.com/?t=" + term + "&y=&plot=short&apikey=trilogy", function (err, response, body) {
+    if (!err && response.statusCode === 200) {
+     
+      console.log("term" + term);
+
+      var information = JSON.parse(body);
+
+
+      console.log(information);
+      // Title of the movie.
+      console.log("title" + information.Title);
+     
+
     }
-      
 
-                  // Title of the movie.
 
-                  // Year the movie came out.
-                  // IMDB Rating of the movie.
-                  // Rotten Tomatoes Rating of the movie.
-                  // Country where the movie was produced.
-                  // Language of the movie.
-                  // Plot of the movie.                    
-          });
-        }
+    
+    
+    // Year the movie came out.
+    console.log("released year: " + information.Year);
+    // IMDB Rating of the movie.
+    console.log("IMDB rating: " + information.imdbRating);
+    // Rotten Tomatoes Rating of the movie.
+    console.log("Rotten Tomatoes: " + information.Ratings[1].value);
+    // Country where the movie was produced.
+    console.log("Produced in: " + information.Country);
+    // Language of the movie.
+    console.log("Language: " + information.Language);
+    // Plot of the movie.   
+    console.log("Plot: " + information.Plot);                 
+  });
+}
